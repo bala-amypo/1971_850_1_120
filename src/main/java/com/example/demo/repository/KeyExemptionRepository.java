6ad759e0b0c1fc7c29804fe7c8a3623989127ce6
bala-amypo <bala@amypo.in>
2025-12-20 09:41:20 +0000

    User commit

--- src/main/java/com/example/demo/repository/KeyExemptionRepository.java
+++ src/main/java/com/example/demo/repository/KeyExemptionRepository.java
@@ -1,4 +1,12 @@
+package com.example.demo.repository;
 
-public interface KeyExemptionRepository extends JpaRepository<KeyExemption, Long> {
+import com.example.demo.entity.KeyExemption;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface KeyExemptionRepository
+        extends JpaRepository<KeyExemption, Long> {
+
     Optional<KeyExemption> findByApiKey_Id(Long id);
 }
--- src/main/java/com/example/demo/repository/RateLimitEnforcementRepository.java
+++ src/main/java/com/example/demo/repository/RateLimitEnforcementRepository.java
@@ -1,4 +1,12 @@
+package com.example.demo.repository;
 
-public interface RateLimitEnforcementRepository extends JpaRepository<RateLimitEnforcement, Long> {
+import com.example.demo.entity.RateLimitEnforcement;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface RateLimitEnforcementRepository
+        extends JpaRepository<RateLimitEnforcement, Long> {
+
     List<RateLimitEnforcement> findByApiKey_Id(Long id);
 }
