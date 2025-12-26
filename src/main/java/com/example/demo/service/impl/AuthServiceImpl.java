edbfe364bf756a278eb03a95d606e4ba7a35e007
bala-amypo <bala@amypo.in>
2025-12-26 08:37:09 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/AuthServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/AuthServiceImpl.java
@@ -9,40 +9,6 @@ import com.example.demo.repository.UserAccountRepository;
 import com.example.demo.security.JwtUtil;
 import com.example.demo.service.AuthService;
 
+@Service
 public class AuthServiceImpl implements AuthService {
-
-    private final UserAccountRepository userRepo;
-    private final JwtUtil jwtUtil;
-
-    public AuthServiceImpl(UserAccountRepository userRepo, JwtUtil jwtUtil) {
-        this.userRepo = userRepo;
-        this.jwtUtil = jwtUtil;
-    }
-
-    @Override
-    public void register(RegisterRequestDto dto) {
-        if (dto.getEmail() == null || dto.getPassword() == null) {
-            throw new BadRequestException("Email and password required");
-        }
-
-        UserAccount user = new UserAccount();
-        user.setEmail(dto.getEmail());
-        user.setPassword(dto.getPassword());
-        user.setRole(dto.getRole());
-
-        userRepo.save(user);
-    }
-
-    @Override
-    public AuthResponseDto login(AuthRequestDto dto) {
-        UserAccount user = userRepo.findByEmail(dto.getEmail())
-                .orElseThrow(() -> new BadRequestException("Invalid credentials"));
-
-        if (!user.getPassword().equals(dto.getPassword())) {
-            throw new BadRequestException("Invalid credentials");
-        }
-
-        String token = jwtUtil.generateToken(user);
-        return new AuthResponseDto(token, user.getRole(), user.getId());
-    }
 }
