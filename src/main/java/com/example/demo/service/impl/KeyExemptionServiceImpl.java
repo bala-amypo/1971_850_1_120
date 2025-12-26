a58bdeade2b2bbfa6d82d2a349a3919048834a84
bala-amypo <bala@amypo.in>
2025-12-26 08:35:41 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/impl/KeyExemptionServiceImpl.java
@@ -0,0 +1,15 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.entity.ApiKey;
+import com.example.demo.exception.BadRequestException;
+import com.example.demo.exception.ResourceNotFoundException;
+import com.example.demo.repository.ApiKeyRepository;
+import com.example.demo.repository.QuotaPlanRepository;
+import com.example.demo.service.ApiKeyService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class KeyExemptionServiceImpl implements KeyExemptionService {
+}
